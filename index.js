8ce40195581810ad1c102fe85ad811e83e5f771c
Joaquin Bernardez <joacob2002@gmail.com>
2022-09-14 22:33:34 -0300

    agrego eventos

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
-class CompraDeProductos {
-    constructor() {
-        this.productos = [
-            parseInt(prompt("Seleccione el numero de producto que desea comprar: \n1-BlackPad ($6000), \n2-Largeblackpad ($7500), \n3-Pinkpad (6500), \nSELECCIONE 0 PARA FINALIZAR SU COMPRA"))
-        ]
-    }
-    seleccionarProducto(){
-      let prod2 = parseInt(prompt("Seleccione el numero de producto que desea comprar: \n1-BlackPad ($6000), \n2-Largeblackpad ($7500), \n3-Pinkpad (6500), \nSELECCIONE 0 PARA FINALIZAR SU COMPRA"))
-      while(prod2 !== 0){
-        if(prod2 > 0 && prod2 <= 3){
-          this.productos.push(prod2)
-          alert(`Se ha agregado el producto ${prod2} correctamente`)
-          prod2 = parseInt(prompt("Seleccione el numero de producto que desea comprar: \n1-BlackPad ($6000), \n2-Largeblackpad ($7500), \n3-Pinkpad (6500), \nSELECCIONE 0 PARA FINALIZAR SU COMPRA"))
-        }
-        else{
-          alert("Producto no existente")
-          prod2 = parseInt(prompt("Seleccione el numero de producto que desea comprar: \n1-BlackPad ($6000), \n2-Largeblackpad ($7500), \n3-Pinkpad (6500), \nSELECCIONE 0 PARA FINALIZAR SU COMPRA"))
-        }
-      }
-      console.log(this.productos)
-    }
+let section = document.getElementById("productos");
+let boton = document.getElementsByClassName("boton");
+class Productos {
+	constructor(id, nombre, precio, propiedades, color, imagen) {
+		this.id = id;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.propiedades = propiedades;
+		this.color = color;
+		this.imagen = [
+			"./images/pad2.webp",
+			"./images/pad3.webp",
+			"./images/pad4.webp",
+		];
+	}
 
-    eliminarProducto(){
-      let eliminar = confirm("Desea eliminar un producto?")
-        while(eliminar){
-          let productoAEliminar = parseInt(prompt(`Tu carrito: ${this.productos}\nIngrese el numero de producto que desea eliminar`))
-          this.productos.splice(this.productos.indexOf(productoAEliminar),1)
-          eliminar = confirm("Desea eliminar un producto?")
-        }
-      }
-    }
+	mostrarProductos() {
+		let div = document.createElement("div");
+		div.innerHTML = `<h2> Nombre: ${this.nombre} </h2> <p> Precio: ${this.precio}</p>
+    <p>Propiedad: ${this.propiedades}</p> <p> Color: ${this.color} </p><img class="cambio"src="${this.imagen[0]}"></img> <button class="boton"> Agregar al carrito </button>`;
+		div.classList.add("productos__card");
+		section.append(div);
+		/* Evento de click */
+		div.addEventListener("click", (a) => {
+			/* div.classList.toggle(
+				alert(
+					"MousePad creado por JCOMPANYY, destinado a la velocidad y a la presicion."
+				)
+			); */
+			div.classList.toggle("primerevento");
+		});
+	}
+	/* agregarAlCarrito() {
+		let arr = [];
+		boton.addEventListener("click", (a) => {
+			boton.push
+		}); */
+}
 
-let general = new CompraDeProductos();
-general.seleccionarProducto();
-general.eliminarProducto();
+let prod1 = new Productos(1, "Pad1", 6000, "Velocidad", "Negro");
+let prod2 = new Productos(2, "Pad2", 8000, "Precision", "Negro");
+let prod3 = new Productos(3, "Pad3", 6500, "Velocidad", "Rosa");
+prod1.mostrarProductos();
+prod2.mostrarProductos();
+prod3.mostrarProductos();
