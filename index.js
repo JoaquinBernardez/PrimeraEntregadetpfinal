638faba008ae9ce6cfd7df797d9f4be63b365dbc
Joaquin Bernardez <joacob2002@gmail.com>
2022-09-21 23:48:09 -0300

    termino pagina

--- index.js
+++ index.js
@@ -1,45 +1,81 @@
-let section = document.getElementById("productos");
-let boton = document.getElementsByClassName("boton");
-class Productos {
-	constructor(id, nombre, precio, propiedades, color, imagen) {
-		this.id = id;
-		this.nombre = nombre;
-		this.precio = precio;
-		this.propiedades = propiedades;
-		this.color = color;
-		this.imagen = [
-			"./images/pad2.webp",
-			"./images/pad3.webp",
-			"./images/pad4.webp",
-		];
-	}
+const contenedorProductos = document.getElementById("contenedor-productos");
 
-	mostrarProductos() {
-		let div = document.createElement("div");
-		div.innerHTML = `<h2> Nombre: ${this.nombre} </h2> <p> Precio: ${this.precio}</p>
-    <p>Propiedad: ${this.propiedades}</p> <p> Color: ${this.color} </p><img class="cambio"src="${this.imagen[0]}"></img> <button class="boton"> Agregar al carrito </button>`;
-		div.classList.add("productos__card");
-		section.append(div);
-		/* Evento de click */
-		div.addEventListener("click", (a) => {
-			/* div.classList.toggle(
-				alert(
-					"MousePad creado por JCOMPANYY, destinado a la velocidad y a la presicion."
-				)
-			); */
-			div.classList.toggle("primerevento");
+let contenedorCarrito = document.getElementById("carrito-contenedor");
+
+let botonVaciar = document.getElementById("vaciar-carrito");
+let contadorCarrito = document.getElementById("contadorCarrito");
+
+let precioTotal = document.getElementById("precioTotal");
+let carrito = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+	if (localStorage.getItem("carrito")) {
+		carrito = JSON.parse(localStorage.getItem("carrito"));
+		actualizarCarrito();
+	}
+});
+
+botonVaciar.addEventListener("click", () => {
+	carrito.length = 0;
+	actualizarCarrito();
+});
+stockDeProductos.forEach((producto) => {
+	let div = document.createElement("div");
+	div.classList.add("producto");
+	div.innerHTML = `
+	<img src=${producto.img} alt="" >
+	<h3 class="producto-h3">${producto.nombre}</h3>
+	<p>${producto.tipo}</p>
+	<p class="precioProducto">Precio: ${producto.precio}</p>
+	<button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="fas fa-shopping-cart"></i></button>
+	`;
+
+	contenedorProductos.appendChild(div);
+
+	let boton = document.getElementById(`agregar${producto.id}`);
+	boton.addEventListener("click", () => {
+		agregarAlCarrito(producto.id);
+	});
+});
+
+let agregarAlCarrito = (prodId) => {
+	let existe = carrito.some((prod) => prod.id === prodId);
+
+	if (existe) {
+		let prod = carrito.map((prod) => {
+			if (prod.id === prodId) {
+				prod.cantidad++;
+			}
 		});
+	} else {
+		let item = stockDeProductos.find((prod) => prod.id === prodId);
+		carrito.push(item);
+		console.log(carrito);
 	}
-	/* agregarAlCarrito() {
-		let arr = [];
-		boton.addEventListener("click", (a) => {
-			boton.push
-		}); */
-}
+	actualizarCarrito();
+};
+let eliminarDelCarrito = (prodId) => {
+	let item = carrito.find((prod) => prod.id === prodId);
+	let indice = carrito.indexOf(item);
+	carrito.splice(indice, 1);
+	actualizarCarrito();
+};
 
-let prod1 = new Productos(1, "Pad1", 6000, "Velocidad", "Negro");
-let prod2 = new Productos(2, "Pad2", 8000, "Precision", "Negro");
-let prod3 = new Productos(3, "Pad3", 6500, "Velocidad", "Rosa");
-prod1.mostrarProductos();
-prod2.mostrarProductos();
-prod3.mostrarProductos();
+let actualizarCarrito = () => {
+	contenedorCarrito.innerHTML = "";
+	carrito.forEach((prod) => {
+		let div = document.createElement("div");
+		div.className = "productoEnCarrito";
+		div.innerHTML = `
+		<p>${prod.nombre}</p>
+		<p>Precio: ${prod.precio}</p>
+		<p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
+		<button onclick = "eliminarDelCarrito(${prod.id})"class="boton-eliminar"><i class="fas fa-trash-alt></button>
+		`;
+		contenedorCarrito.appendChild(div);
+
+		localStorage.setItem("carrito", JSON.stringify(carrito));
+	});
+	contadorCarrito.innerText = carrito.length;
+	precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0);
+};
