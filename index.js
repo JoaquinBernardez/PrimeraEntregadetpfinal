899bb0761528eb03c67de8caca87c2c0a4a66d75
Joaquin Bernardez <joacob2002@gmail.com>
2022-10-04 22:25:10 -0300

    Agrego fetch a mi pagina

--- index.js
+++ index.js
@@ -7,6 +7,49 @@ let carritoCerrar = document.getElementById("carritoCerrar");
 let precioTotal = document.getElementById("precioTotal");
 let carrito = [];
 
+document.addEventListener("DOMContentLoaded", async () => {
+	let promesa = await fetch("./productos.json");
+	let jsonPromesa = await promesa.json();
+	let stockDeProductos = jsonPromesa.producto;
+	stockDeProductos.forEach((producto) => {
+		let div = document.createElement("div");
+		div.classList.add("producto");
+		div.innerHTML = `
+		<img src=${producto.img} alt="" >
+		<h3 class="producto-h3">${producto.nombre}</h3>
+		<p>${producto.tipo}</p>
+		<p class="precioProducto">Precio: ${producto.precio}</p>
+		<button id="agregar${producto.id}" class="boton-agregar p-2 rounded">Agregar <i class="fas fa-shopping-cart"></i></button>
+		`;
+		contenedorProductos.appendChild(div);
+
+		let boton = document.getElementById(`agregar${producto.id}`);
+		boton.addEventListener("click", () => {
+			agregarAlCarrito(producto.id);
+			tostadaAgregar(producto.nombre);
+		});
+		let agregarAlCarrito = (prodId) => {
+			let existe = carrito.some((prod) => prod.id === prodId);
+
+			if (existe) {
+				let prod = carrito.map((prod) => {
+					if (prod.id === prodId) {
+						prod.cantidad++;
+					}
+				});
+			} else {
+				let item = stockDeProductos.find((prod) => prod.id === prodId);
+				carrito.push(item);
+			}
+			actualizarCarrito();
+		};
+	});
+	if (localStorage.getItem("carrito")) {
+		carrito = JSON.parse(localStorage.getItem("carrito"));
+		actualizarCarrito();
+	}
+});
+
 let actualizarCarrito = () => {
 	contenedorCarrito.innerHTML = "";
 	if (carrito.length > 0) {
@@ -43,52 +86,10 @@ let eliminarDelCarrito = (prodId) => {
 	tostadaEliminar(item.nombre);
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-	if (localStorage.getItem("carrito")) {
-		carrito = JSON.parse(localStorage.getItem("carrito"));
-		actualizarCarrito();
-	}
-});
-
 botonVaciar.addEventListener("click", () => {
 	carrito.length = 0;
 	actualizarCarrito();
 });
-
-stockDeProductos.forEach((producto) => {
-	let div = document.createElement("div");
-	div.classList.add("producto");
-	div.innerHTML = `
-	<img src=${producto.img} alt="" >
-	<h3 class="producto-h3">${producto.nombre}</h3>
-	<p>${producto.tipo}</p>
-	<p class="precioProducto">Precio: ${producto.precio}</p>
-	<button id="agregar${producto.id}" class="boton-agregar p-2 rounded">Agregar <i class="fas fa-shopping-cart"></i></button>
-	`;
-	contenedorProductos.appendChild(div);
-
-	let boton = document.getElementById(`agregar${producto.id}`);
-	boton.addEventListener("click", () => {
-		agregarAlCarrito(producto.id);
-		tostadaAgregar(producto.nombre);
-	});
-});
-
-let agregarAlCarrito = (prodId) => {
-	let existe = carrito.some((prod) => prod.id === prodId);
-
-	if (existe) {
-		let prod = carrito.map((prod) => {
-			if (prod.id === prodId) {
-				prod.cantidad++;
-			}
-		});
-	} else {
-		let item = stockDeProductos.find((prod) => prod.id === prodId);
-		carrito.push(item);
-	}
-	actualizarCarrito();
-};
 
 function tostadaAgregar(productoNombre) {
 	Toastify({
